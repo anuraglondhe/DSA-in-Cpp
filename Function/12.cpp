72f4811fb5b9e3e2606c024f5a70b745640fec95
Anurag Londhe <anuraglondhe9052@gmail.com>
2026-03-30 15:41:55 +0530

    Function Practice

---
+++ Function/12.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int sumOfDig(int num){
+
+    int DigSum = 0;    
+    while(num > 0){
+        int lastDig = num%10;
+        num = num/10;
+
+        DigSum += lastDig;        
+    }
+    return DigSum;
+}
+
+int main(){
+    int num = 2549;
+    cout<<"Sum of 2549 :"<<sumOfDig(num)<<endl;
+    return 0;
+}
